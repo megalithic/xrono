16c69f5e0d2ae5627ff5777f5a6c067225c18727
Josh Adams <josh@isotope11.com>
2012-10-24 17:03:19 -0500

    Get bundle installing with rails4...there are some local gems involved.

--- app/models/data_vault.rb
+++ app/models/data_vault.rb
@@ -1,5 +1,5 @@
 class DataVault < ActiveRecord::Base
-  has_paper_trail
+  #has_paper_trail
   attr_encrypted :data, :key => "SECRET!", :attribute => :encrypted_data
 
   belongs_to :data_vaultable, :polymorphic => true
